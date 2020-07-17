0e2579e9e0e8c32cdcbbbb037d43cce8a4afb60e
alokmaurya05 <alokkumarmaurya1200@gmail.com>
2020-07-17 20:05:31 +0200

    For word is added

--- FeatureBranch.java
+++ FeatureBranch.java
@@ -4,7 +4,7 @@ public class FeatureBranch {
 
 	public static void main(String[] args) 
 	{	
-	System.out.println("Demo Just for Testing perpose of FeatureBranch");
+	System.out.println("Demo Just For Testing perpose of FeatureBranch");
 	}
 
 }
